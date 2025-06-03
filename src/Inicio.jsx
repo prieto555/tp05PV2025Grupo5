9879730102b1cb7837e3dffc771b5c42796943cf
prieto555 <prietogabriel555@gmail.com>
2025-06-03 10:04:51 -0300

    Arreglando desperfectos por Gabriel Prieto

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Inicio = () => {
   return (
-   <div className="container text-center mt-5">
+    <div className="container text-center mt-5">
       <div className="card p-4 shadow-lg">
         <h1 className="mb-3 text-primary">Bienvenido a la Gestión de Alumnos</h1>
         <p className="lead">Explora las distintas páginas para administrar alumnos de manera eficiente.</p>
-        <a href="/mostrar" className="btn btn-success mt-3">Ver Lista de Alumnos</a>
+        <Link className="btn btn-success mt-3" to='/mostrar'>Ver Lista de Alumnos</Link>
       </div>
     </div>
   );
